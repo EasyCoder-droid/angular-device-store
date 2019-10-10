7c0065952f6634d50ad5cfebd32cd82fc3c925e6
EasyCoder-droid <master4321go@gmail.com>
2019-10-10 07:52:05 +0530

    3 files modified, 3 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -12,6 +12,11 @@ export const products = [
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: 'A standard, device phone, not built for games, just for media and social apps'
+  },
+  {
+    name: 'Super Sonic HeadPhones',
+    price: 159,
+    description: 'A super ultrasounding headphones that are great for gaming and hd videos'
   }
 ];
